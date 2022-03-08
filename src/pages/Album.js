874d2fcd5e2ef5f3fa6816f850b5ebd08566ab33
gitdoallan <mailtoallan@icloud.com>
2022-03-08 11:50:49 -0300

    commitando antes de reiniciar aqui

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import PropType, { string } from 'prop-types';
+import getMusics from '../services/musicsAPI';
 
 export default class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
+
+  async listSongs(id) {
+    const result = await getMusics(id);
+    // this.setState({ data: result });
+  }
+
   render() {
+    const { match: { params: { id } } } = this.props;
+    const { data } = this.state;
+    this.listSongs(id);
     return (
       <div data-testid="page-album">
         <span>Página: Album!!!!</span>
@@ -9,3 +26,11 @@ export default class Album extends React.Component {
     );
   }
 }
+
+Album.propTypes = {
+  match: PropType.shape({
+    params: PropType.shape({
+      id: string,
+    }),
+  }).isRequired,
+};
